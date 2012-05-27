0cdcfc4e8dd89bb08c66e871a639975b99f37982
Daniel <daniel@daniel.(none)>
2012-05-27 09:51:07 -0400

    Restructured app a little created tests dir, renamed website to pages

--- ourdata/__init__.py
+++ ourdata/__init__.py
@@ -10,7 +10,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
 
-    # views.website.py
+    # views.pages.py
     config.add_route('home', '/')
     config.add_route('signup', '/signup')
     config.add_route('dashboard', '/dashboard')
--- ourdata/tests/test_pages.py
+++ ourdata/tests/test_pages.py
@@ -41,28 +41,3 @@ class ViewFunctionalTests(unittest.TestCase):
         new_count = User.objects.all().count()
         self.assertEqual(new_count, old_count + 1)
         #import ipdb; ipdb.set_trace()
-
-
-class UtilsTests(unittest.TestCase): 
-    """ Tests utils.py functions."""
-    
-    def test_email_validate(self):
-        """
-        Verify that an incorrect email can be detected,
-        and a valid one can be detected.
-        """
-        from ourdata.utils import validate_email
-        from ourdata.exceptions import ValidationError
-
-        #import ipdb; ipdb.set_trace()
-        valid_email = 'test@test.com'
-        invalid_email = 'testtest.com'
-
-        validate_email(valid_email)
-        try:
-            validate_email(invalid_email)
-            self.fail('invalid email not caught')
-        except ValidationError:
-            pass
-            
-            
